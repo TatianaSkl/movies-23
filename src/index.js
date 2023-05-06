41f72207110434edb97e0f7f3719774c193db6cd
VadymMiezin <vadym.miezin@gmail.com>
2023-05-06 15:26:19 +0300

    message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.use(express.logger());
+app.set('views', __dirname + '/templates');
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.send('hello world');
 });
