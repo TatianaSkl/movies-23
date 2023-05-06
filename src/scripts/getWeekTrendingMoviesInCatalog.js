b684614a9104a1a21a1da155992d58a2b737986b
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-05-06 15:31:19 +0300

    Update catalog files

--- src/scripts/getWeekTrendingMoviesInCatalog.js
+++ src/scripts/getWeekTrendingMoviesInCatalog.js
@@ -1,19 +1,37 @@
-import { createMarkupFilmsList } from "./markup";
-import { CatalogApiService } from "./movies-api";
+import { createMarkupFilmsList } from './markup';
+import { CatalogApiService } from './movies-api';
 
-const movieList = document.querySelector('.catalog__list')
+const movieList = document.querySelector('.catalog__list');
 
-const movieAPI = new CatalogApiService()
+const movieAPI = new CatalogApiService();
 
-export function renderTrendMovie() {
-    movieAPI.getWeekTrendingMoviesInCatalog().then(data => {
-        const markupTrendMovies = createMarkupFilmsList(data)
+// export function renderTrendMovie() {
+//   movieAPI
+//     .getWeekTrendingMoviesInCatalog()
+//     .then(data => {
+//       const markupTrendMovies = createMarkupFilmsList(data);
 
-        console.log(data)
-        movieList.innerHTML = markupTrendMovies;
-    }).catch(error => {
-        console.log(error)
-    })
+//       console.log(data);
+//       movieList.innerHTML = markupTrendMovies;
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
+
+export async function renderTrendMovie() {
+  const data = await movieAPI.getWeekTrendingMoviesInCatalog();
+  const results = await data.results;
+  console.log(results);
+
+  try {
+    if (results !== 0) {
+      const markupTrendMovies = createMarkupFilmsList(results);
+      movieList.insertAdjacentHTML('beforeend', markupTrendMovies);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-renderTrendMovie()
+renderTrendMovie();
--- src/scripts/markup.js
+++ src/scripts/markup.js
@@ -1,18 +1,19 @@
 export function createMarkupFilmsList(moviesData) {
-    return moviesData.map(movie => {
-        const { genres, id, poster_path, title, vote_average, release_date } = movie;
+  return moviesData.map(movie => {
+    const { genres, id, poster_path, title, vote_average, release_date } =
+      movie;
     return `<li class="film-card" data-id=${id}> 
             <img class="film-cardimg" src='${`https://image.tmdb.org/t/p/w500${poster_path}`}' alt="film" /> 
  
             <div class="film-cardwrapper"> 
               <h2 class="film-cardtitle">${title}</h2> 
               <ul class="film-carddescription list"> 
-                <li class="film-cardgenres">${genres}</li> 
+                <li class="film-cardgenres">${genres}  |</li> 
  
                 <li class="film-cardrelease-date">${release_date}</li> 
               </ul> 
               <span class="film-card__rating">${vote_average.toFixed(1)}</span> 
             </div> 
-          </li>`
-        })
+          </li>`;
+  });
 }
