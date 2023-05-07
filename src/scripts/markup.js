ac2b0a23ea8b77328640925d55e3f10689c07c8d
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-05-07 15:11:49 +0300

    Create markup.js

---
+++ src/scripts/markup.js
@@ -0,0 +1,41 @@
+export function createMarkupFilmsList(moviesData) {
+  return moviesData
+    .map(movie => {
+      const { genres, id, poster_path, title, vote_average, release_date } =
+        movie;
+      return `<li class="film-card" data-id="${id}">
+  <img
+    class="film-card__img"
+    src="${`https://image.tmdb.org/t/p/w500${poster_path}`}"
+    alt="film"
+  />
+
+  <div class="film-card__wrapper">
+    <h2 class="film-card__title">${title}</h2>
+    <ul class="film-card__description list">
+      <li class="film-card__genres">${genres} |</li>
+
+      <li class="film-card__release-date">${release_date}</li>
+    </ul>
+    <span class="film-card__rating">
+      <div class="rating">
+        <div class="rating__body">
+          <div class="rating__active"></div>
+          <div class="rating__items">
+            <input
+              type="range"
+              min="0"
+              max="10"
+              class="rating__item"
+              value="${vote_average}"
+              name="rating"
+            />
+          </div>
+        </div>
+      </div>
+    </span>
+  </div>
+</li>`;
+    })
+    .join('');
+}
