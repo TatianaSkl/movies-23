e13bba2790a9769f14eb156b6279e88725e9c86e
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-05-07 10:43:41 +0000

    Deploying to gh-pages from @ TatianaSkl/movies-23@23aebc08100a196047db42b4e108017e7935358f 🚀

--- my-library.0d6d3bbc.js
+++ my-library.0d6d3bbc.js
@@ -1,2 +1,2 @@
 $(".back-to-top").click((function(){$("body,html").animate({scrollTop:0},800)})),$(window).scroll((function(){$(window).scrollTop()>350?$(".back-to-top").addClass("active"):$(".back-to-top").removeClass("active")}));
-//# sourceMappingURL=index.bd76feaf.js.map
+//# sourceMappingURL=my-library.0d6d3bbc.js.map
