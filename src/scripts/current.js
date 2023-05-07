175e714ccdaac9b02fbe6871591d6c1e19c2aa2d
Oleh <choleg22@gmail.com>
2023-05-07 18:59:50 +0300

    добавив неон для логотипу

--- src/scripts/current.js
+++ src/scripts/current.js
@@ -1,18 +1,18 @@
-// const navLinks = document.querySelectorAll('.site-nav__link');
+const navLinks = document.querySelectorAll('.site-nav__link');
 
-// navLinks.forEach(function(navLink) {
-//   navLink.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     if (this.classList.contains('current')) {
-//       return;
-//     }
+navLinks.forEach(function(navLink) {
+  navLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (this.classList.contains('current')) {
+      return;
+    }
 
-//     const currentItems = document.querySelectorAll('.site-nav__link.current');
-//     currentItems.forEach(function(currentItem) {
-//       currentItem.classList.remove('current');
-//     });
+    const currentItems = document.querySelectorAll('.site-nav__link.current');
+    currentItems.forEach(function(currentItem) {
+      currentItem.classList.remove('current');
+    });
       
-//     this.classList.add('current');
+    this.classList.add('current');
 
-//   });
-// });
+  });
+});
