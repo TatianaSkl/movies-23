907e1c52d08a39899b8ca691ac1fed22a559054b
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-05-08 10:52:03 +0200

    модалка в хіроу, робочи проміжний варіант
    мені здається може бути конфлікт в індексі

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { renderTrendMovie } from "./scripts/getWeekTrendingMoviesInCatalog";
 import { onSearch } from "./scripts/search-films";
 import home from './scripts/home-scripts'
+import {hero} from './scripts/hero-modal';
