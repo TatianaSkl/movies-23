d4ca28dde1b21b86cabb2e5ed34b54dfe626a872
Oleh <choleg22@gmail.com>
2023-05-08 22:43:22 +0300

    додав перемикач світлого і темного режимів

---
+++ src/scripts/switch-theme.js
@@ -0,0 +1,27 @@
+const switchBtn = document.querySelector('.switch-theme-button');
+const bodyEL = document.body;
+
+function changeTheme() {
+  if (bodyEL.classList.contains('light')) {
+    bodyEL.classList.remove('light');
+    localStorage.removeItem('site-theme');
+  } else {
+    bodyEL.classList.add('light');
+    localStorage.setItem('site-theme', 'light');
+  }
+}
+
+switchBtn.addEventListener('click', changeTheme);
+
+const currentTheme = localStorage.getItem('site-theme');
+
+if (currentTheme) {
+bodyEL.classList.add(currentTheme);
+}
+
+
+
+
+
+
+
