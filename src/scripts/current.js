7613fcaf50fe25b5e4b28b7f54e5eeb8bfbbe2f2
Oleh <choleg22@gmail.com>
2023-05-09 19:55:35 +0300

    написав 3 окремих скрипти для переходу по секціях

--- src/scripts/current.js
+++ src/scripts/current.js
@@ -1,18 +1,18 @@
 // const navLinks = document.querySelectorAll('.site-nav__link');
+// console.log('🚀 ~ file: current.js:2 ~ navLinks:', navLinks);
 
-// navLinks.forEach(function(navLink) {
-//   navLink.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     if (this.classList.contains('current')) {
+// navLinks.forEach(function (navLink) {
+//   navLink.addEventListener('click', function (event) {
+//     // event.preventDefault();
+//     if (navLink.classList.contains('current')) {
 //       return;
 //     }
 
 //     const currentItems = document.querySelectorAll('.site-nav__link.current');
-//     currentItems.forEach(function(currentItem) {
+//     currentItems.forEach(function (currentItem) {
 //       currentItem.classList.remove('current');
 //     });
-      
-//     this.classList.add('current');
 
+//     navLink.classList.add('current');
 //   });
 // });
