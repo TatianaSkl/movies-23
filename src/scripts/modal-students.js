66a4fdf6445a69a680e64f41a1a16ccc0e2b3bb2
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-05-12 14:14:27 +0300

    Update students-modal

--- src/scripts/modal-students.js
+++ src/scripts/modal-students.js
@@ -1,25 +1,33 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    modalBackdrop: document.querySelector('[data-modal-backdrop]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modalBackdrop.addEventListener('click', onBackdropClick);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    const body = document.querySelector("body");
-    body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('is-hidden');
+    const body = document.querySelector('body');
+    body.classList.toggle('modal-open');
 
-    document.addEventListener("keydown", onDocumentKeyDown);
+    document.addEventListener('keydown', onDocumentKeyDown);
   }
 
   function onDocumentKeyDown(event) {
-    if (event.key === "Escape" && !refs.modal.classList.contains("is-hidden")) {
+    if (event.key === 'Escape' && !refs.modal.classList.contains('is-hidden')) {
       toggleModal();
-      document.removeEventListener("keydown", onDocumentKeyDown);
+      document.removeEventListener('keydown', onDocumentKeyDown);
+    }
+  }
+
+  function onBackdropClick(event) {
+    if (event.target === refs.modalBackdrop) {
+      toggleModal();
     }
   }
 })();
