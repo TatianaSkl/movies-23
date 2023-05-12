f095ecca1d6e950cc7eddf0275db41d380fffc3b
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-05-12 10:04:23 +0300

    Update js

--- src/scripts/modal-students.js
+++ src/scripts/modal-students.js
@@ -1,20 +1,3 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//     const body = document.querySelector('body');
-//   body.classList.toggle('modal-open');
-//   }
-// })();
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -30,14 +13,12 @@
     const body = document.querySelector("body");
     body.classList.toggle("modal-open");
 
-    // Додайте обробник події keydown на весь документ
     document.addEventListener("keydown", onDocumentKeyDown);
   }
 
   function onDocumentKeyDown(event) {
     if (event.key === "Escape" && !refs.modal.classList.contains("is-hidden")) {
       toggleModal();
-      // Видалити обробник події keydown
       document.removeEventListener("keydown", onDocumentKeyDown);
     }
   }
