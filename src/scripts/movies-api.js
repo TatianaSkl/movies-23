f56b64e21fcefdb4430282509ea42e3db9ea7330
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-06-21 17:45:10 +0300

    add key

--- src/scripts/movies-api.js
+++ src/scripts/movies-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '2c2e924badab09018363b97151da7944';
+const API_KEY = '214e13c97c55baca13ab56614ecbad3f';
 
 class CatalogApiService {
   constructor() {
@@ -32,44 +32,32 @@ class CatalogApiService {
 }
 
 async function getMovieDetails(movieId) {
-  const response = await axios.get(
-    `${BASE_URL}movie/${movieId}?api_key=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}movie/${movieId}?api_key=${API_KEY}`);
   return response.data;
 }
 
 async function getMovieVideos(movieId) {
-  const response = await axios.get(
-    `${BASE_URL}movie/${movieId}/videos?api_key=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}movie/${movieId}/videos?api_key=${API_KEY}`);
   return response.data;
 }
 
 async function getUpcomingMovies() {
-  const response = await axios.get(
-    `${BASE_URL}movie/upcoming?api_key=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}movie/upcoming?api_key=${API_KEY}`);
   return response.data;
 }
 
 async function getWeekTrendingMovies() {
-  const response = await axios.get(
-    `${BASE_URL}trending/movie/week?api_key=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}trending/movie/week?api_key=${API_KEY}`);
   return response.data;
 }
 
 async function getDayTrendingMovies() {
-  const response = await axios.get(
-    `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}trending/movie/day?api_key=${API_KEY}`);
   return response.data;
 }
 
 async function getGenres() {
-  const response = await axios.get(
-    `${BASE_URL}genre/movie/list?api_key=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}genre/movie/list?api_key=${API_KEY}`);
   return response.data;
 }
 
